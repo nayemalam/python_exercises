1cd19d019e5f1fc658de260ee11ea73d1584a9c4
Nayem <nayem.alam@mail.mcgill.ca>
2018-02-16 00:44:46 -0500

    testing commit

--- TestingforGit.py
+++ TestingforGit.py
@@ -1,1 +1,13 @@
 print("Hellow World!")
+print("New print testing")
+
+#Finding the max value in an array
+someArray = [1,2203,983,224]
+max = 0
+for i in range(len(someArray)):
+
+    if (someArray[i] > max):
+        max = someArray[i]
+
+print(max)
+
